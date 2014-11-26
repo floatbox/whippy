40ece1f3840a851a7c70de8094ea8c81b2b26d85
Andrey Arefiev <arefiev@gmail.com>
2014-11-26 11:10:07 +0300

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whippy::Application.config.secret_token = 'd24366c01420d65b14b0715ff30c142f7ac3d2b5a0b524ecca4bc0ef7e3ede696f8943df387f095e9ba23e81ac7dd4a3f5a2e2c64fc66b84295e30bbfc97c0a7'
